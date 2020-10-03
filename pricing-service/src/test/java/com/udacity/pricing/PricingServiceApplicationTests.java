d893d979e3fd1188986daf0bb41cb6601c375653
oguzhan.unalir <oguzhanunalir@hotmail.com>
2020-10-03 23:36:18 +0300

    commit

---
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -0,0 +1,35 @@
+package com.udacity.pricing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PricingServiceApplicationTests {
+	@Autowired
+	private MockMvc mockMvc;
+
+
+	@Test
+	public void contextLoads() {
+	}
+
+
+	@Test
+	public void getAllPrices() throws Exception {
+		mockMvc.perform(get("/prices"))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType("application/hal+json;charset=UTF-8"));
+
+	}
+
+}
